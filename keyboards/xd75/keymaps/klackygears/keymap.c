ee8e14df39013c505b9945e9b4164d9ed452c7ec
klackygears <bronzegears@gmail.com>
2019-08-08 21:33:18 -0400

    UPATING XD75

--- keyboards/xd75/keymaps/klackygears/keymap.c
+++ keyboards/xd75/keymaps/klackygears/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_MACBASE] = LAYOUT_wrapper( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TO(_GAMER),
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TO(_WINBASE),
     _______, _______________DVORAK_L1___________________, LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_RIGHT), _______________DVORAK_R1___________________, _______,
     _______, _______________DVORAK_L2___________________, KC_MUTE, KC_VOLD, KC_VOLU, _______________DVORAK_R2___________________, KC_ENT,
     KC_LSFT, _______________MACDVK_L3___________________, KC_PGUP, KC_UP,   KC_PGDN, _______________MACDVK_R3___________________, KC_RSFT,
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_WINBASE] = LAYOUT_wrapper( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TO(_GAMER),
     _______, _______________DVORAK_L1___________________, LCTL(KC_TAB), LGUI(KC_TAB), LCTL(LSFT(KC_TAB)), _______________DVORAK_R1___________________, _______,
     _______, _______________DVORAK_L2___________________, KC_MUTE, KC_VOLD, KC_VOLU, _______________DVORAK_R2___________________, _______,
     _______, _______________WINDVK_L3___________________, KC_PGUP, KC_UP,   KC_PGDN, _______________WINDVK_R3___________________, _______,
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_GAMER] = LAYOUT_wrapper( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_MINS, KC_PLUS, KC_QUES,  _________________KC_BLANK__________________,    KC_MACBASE,
-    KC_TAB,  _________________QWERTY_L1_________________,    KC_7, KC_8, KC_9, _________________QWERTY_R1_________________, _______,
-    KC_CAPS, _________________QWERTY_L2_________________,    KC_4, KC_5, KC_6, _________________QWERTY_R2_________________, _______,
-    KC_LSFT, _________________QWERTY_L3_________________,    KC_1, KC_2, KC_3, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
-    KC_LCTL, KC_LALT, _______, KC_LCTL, MT(MOD_LSFT,KC_SPC), KC_SPC, KC_UP, KC_DOWN, KC_0, _________________KC_BLANK__________________, MO(_FUNC)
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_PLUS, KC_QUES, KC_6,    KC_7,    KC_8,    KC_9,    KC_0, TO(_WINBASE),
+    KC_TAB,  _________________QWERTY_L1_________________, KC_7, KC_8, KC_9, _________________QWERTY_R1_________________, _______,
+    KC_LCTL, _________________QWERTY_L2_________________, KC_4, KC_5, KC_6, _________________QWERTY_R2_________________, _______,
+    KC_LSFT, _________________QWERTY_L3_________________, KC_1, KC_2, KC_3, _________________QWERTY_R3_________________, _______,
+    KC_LCTL, KC_LALT, KC_LCTL, KC_LSFT, KC_SPC, KC_BSPC,  KC_UP, KC_DOWN, KC_0, _________________KC_BLANK__________________, MO(_FUNC)
   ),
 
 
@@ -66,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_FUNC] = LAYOUT_wrapper( \
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,
-    _______, _____________FUNC_L1_______________________, _______, _______, _______, _______, _____________FUNC_1_______________, _______,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    TO(_WINBASE), _____________FUNC_L1_______________________, _______, _______, _______, _______, _____________FUNC_1_______________, _______,
     _______, ________MAC_MISSION_CTRL__________, LGUI(KC_L), _______, _______, _______, _______, _____________FUNC_2_______________, _______,
     _______, _____________FUNC_L3_______________________, _______, _______, _______, KC_GRV,  _____________FUNC_3_______________, _______,
-    _________________KC_BLANK__________________, _________________KC_BLANK__________________, _________________KC_BLANK__________________
+    _________________KC_BLANK__________________, _______, _______, _______, _______, KC_CAPS, _________________KC_BLANK__________________
   ),
 
 
