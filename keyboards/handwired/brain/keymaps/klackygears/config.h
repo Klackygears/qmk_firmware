c5f1a211d3777a048c3626b90bfd8c589a571727
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-05-22 11:48:06 -0400

    adding RGB twinkle to Brain

--- keyboards/handwired/brain/keymaps/klackygears/config.h
+++ keyboards/handwired/brain/keymaps/klackygears/config.h
@@ -45,5 +45,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //   #define RGBLIGHT_EFFECT_RGB_TEST
 //   #define RGBLIGHT_EFFECT_ALTERNATING
-
-
+   #define RGBLIGHT_EFFECT_TWINKLE
+    #define RGBLIGHT_EFFECT_TWINKLE_PROBABILITY 1 / 127
