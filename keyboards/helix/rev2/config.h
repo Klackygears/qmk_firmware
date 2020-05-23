5f5f8ae60801f76d0a49117350d9214b6983f94e
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-05-22 23:02:57 -0400

    adding matrix code

--- keyboards/helix/rev2/config.h
+++ keyboards/helix/rev2/config.h
@@ -86,6 +86,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
+#define DRIVER_LED_TOTAL 64
 
 //#define RGBLED_NUM 12    // Number of LEDs. see ./keymaps/default/config.h
 
