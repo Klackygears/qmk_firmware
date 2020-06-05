ee5c74c4c1c35a27add6e7af7cf03b1fdc0a1f7f
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-06-05 08:51:49 -0400

    messing with audio and file size

--- keyboards/eek/keymaps/klackygears/rules.mk
+++ keyboards/eek/keymaps/klackygears/rules.mk
@@ -20,3 +20,6 @@ HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
 TAP_DANCE_ENABLE = yes
 RGB_MATRIX_ENABLE = WS2812
  OPT_DEFS += -DFLIPPED      # Enable if board is flipped to have eek! silk on top
+#AUDIO_ENABLE = yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+
