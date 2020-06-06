abb0415134da544bfbaf95e8250a9560fa8f4554
James Smith <37446827+Klackygears@users.noreply.github.com>
2020-06-05 22:33:24 -0400

    Revert "Revert "Klackygears user improvements""

--- keyboards/handwired/brain/keymaps/klackygears/config.h
+++ keyboards/handwired/brain/keymaps/klackygears/config.h
@@ -47,3 +47,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //   #define RGBLIGHT_EFFECT_ALTERNATING
    #define RGBLIGHT_EFFECT_TWINKLE
     #define RGBLIGHT_EFFECT_TWINKLE_PROBABILITY 1 / 127
+
