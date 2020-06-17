571ef5666e32ef89e5fbd29a2115ef4fdf01aa38
James Smith <37446827+Klackygears@users.noreply.github.com>
2020-06-17 07:55:44 -0400

    Update keyboards/handwired/brain/keymaps/klackygears/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/brain/keymaps/klackygears/keymap.c
+++ keyboards/handwired/brain/keymaps/klackygears/keymap.c
@@ -39,12 +39,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      KC_NO,      KC_LSFT,  KC_LSFT,        KC_LEFT, KC_DOWN, KC_RIGHT
   ),
   //Borderlands
-  [_GAMR1] = LAYOUT_wrapper( \
-             KC_R,    KC_G,    KC_W,    KC_E,    KC_1,      KC_ESC,        KC_WINBASE, _________________RGB_1_____________________, \
-    KC_TAB,  KC_V,    KC_A,    KC_S,    KC_D,    KC_2,        KC_L,        KC_GAMR1, _________________RGB_2_____________________, RGB_TOG, \
-    KC_T,    KC_I,    KC_F,    KC_Q,    KC_4,    KC_3,        KC_M,        KC_GAMR2, _________________MEDIA_____________________, RGB_MOD, \
-             KC_X,    KC_K,             KC_C,    KC_LSFT,   KC_SPC,        KC_____, KC_UP,   KC_____,           KC_____, KC_____, \
-                                        KC_Z,    KC_LSFT,  KC_LSFT,        KC_LEFT, KC_DOWN, KC_RIGHT \
+  [_GAMR1] = LAYOUT_wrapper(
+             KC_R,    KC_G,    KC_W,    KC_E,    KC_1,      KC_ESC,        KC_WINBASE, _________________RGB_1_____________________,
+    KC_TAB,  KC_V,    KC_A,    KC_S,    KC_D,    KC_2,        KC_L,        KC_GAMR1, _________________RGB_2_____________________, RGB_TOG,
+    KC_T,    KC_I,    KC_F,    KC_Q,    KC_4,    KC_3,        KC_M,        KC_GAMR2, _________________MEDIA_____________________, RGB_MOD,
+             KC_X,    KC_K,             KC_C,    KC_LSFT,   KC_SPC,        KC_____, KC_UP,   KC_____,           KC_____, KC_____,
+                                        KC_Z,    KC_LSFT,  KC_LSFT,        KC_LEFT, KC_DOWN, KC_RIGHT
   ),
   //Witcher
   [_GAMR2] = LAYOUT_wrapper( \
