8adad59176e4bc13c704422a1ddf2cbe8e58d35f
Klackygears <37446827+Klackygears@users.noreply.github.com>
2021-03-03 13:55:16 -0500

    Adding EIN_60

---
+++ keyboards/ein60/keymaps/ledtest/rules.mk
@@ -0,0 +1,3 @@
+#RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes       # Enable for pretty RGB matrix effects
+OLED_DRIVER_ENABLE = yes      # Enable for OLED Support
