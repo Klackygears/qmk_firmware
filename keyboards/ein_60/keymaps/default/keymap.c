e00fc3c85fa47e04c91e500a80f1e2fd2b25f9d9
Klackygears <37446827+Klackygears@users.noreply.github.com>
2021-09-10 13:05:06 -0400

    making some default changes

--- keyboards/ein_60/keymaps/default/keymap.c
+++ keyboards/ein_60/keymaps/default/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  _______,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
      KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,              _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
      KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             _______, _______, _______, KC_MUTE, _______, KC_PIPE,
-     _______, _______,  _______, _______, _______, KC_BSPC,  _______,  KC_BSPC, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+     _______, _______,  _______, _______, KC_NO,   KC_BSPC,  _______,  KC_BSPC, KC_NO,   KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
     ),
     /* Raise
      * ,-----------------------------------------.  ,------.  ,-----------------------------------------.
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,  KC_1,    KC_2,  KC_3,   KC_4,    KC_5,     _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
      _______, KC_4,    KC_5,  KC_6,   KC_PLUS, KC_F5,              KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
      KC_ENT,  KC_7,    KC_8,  KC_9,   KC_MINS, KC_F11,             KC_F12,  KC_NUHS, KC_NUBS, KC_MUTE, _______, KC_BSLS,
-     _______, KC_COMM, KC_0,  KC_DOT, _______, KC_BSPC,  _______,  KC_BSPC, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+     _______, KC_COMM, KC_0,  KC_DOT, KC_NO,  KC_BSPC,  _______,  KC_BSPC, KC_NO,   KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
     ),
     /* Adjust (Lower + Raise)
      * ,-----------------------------------------.  ,------.  ,-----------------------------------------.
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_VAI, RGB_SAI, RGB_HUI, _______,
      _______, _______, _______, _______, _______, _______,          _______, RGB_RMOD,RGB_VAD, RGB_SAD, RGB_HUD, RGB_TOG,
      _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, BL_STEP,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET
+     _______, _______, _______, _______, KC_NO,   _______, _______, _______, KC_NO,   _______, _______, _______, RESET
     ),
     /* Function
      * ,-----------------------------------------.  ,------.  ,-----------------------------------------.
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______,
      _______, _______, _______, _______, _______, _______,          _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
      KC_CAPS, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+     _______, _______, _______, _______, KC_NO,   _______, _______, _______, KC_NO,   _______, _______, _______, _______
     )
 };
 
