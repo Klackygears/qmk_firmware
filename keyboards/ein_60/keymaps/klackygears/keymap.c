90e02e9903961a06f6912e8f98c9b43b5096eb54
Klackygears <37446827+Klackygears@users.noreply.github.com>
2021-09-16 13:52:58 -0400

    replacing klackygears user and updating ein_60 keymap to include combos after rebasing

--- keyboards/ein_60/keymaps/klackygears/keymap.c
+++ keyboards/ein_60/keymaps/klackygears/keymap.c
@@ -21,25 +21,25 @@
 //For an explanation of what's going on here with the keymap wrappers, check out drashna's user folder.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_WINBASE] = LAYOUT_split_4x6_2_wrapper(
+    [_WINBASE] = LAYOUT_wrapper(
       KC_ESC,  _______________DVORAK_L1___________________,   KC_MUTE,  _______________DVORAK_R1___________________, KC_BSPC,
       AU_TOG,  _______________DVORAK_L2___________________,             _______________DVORAK_R2___________________, RGB_TOG,
       CK_TOGG, _______________WINDVK_L3___________________,             _______________WINDVK_R3___________________, RGB_MOD,
       KC_1,  KC_2, KC_3, ________WIN_THUMB_L______,   KC_SPC,   ________WIN_THUMB_R______, KC_1,  KC_2, KC_GAMER
     ),
-    [_MACBASE] = LAYOUT_split_4x6_2_wrapper(
+    [_MACBASE] = LAYOUT_wrapper(
       KC_ESC,  _______________DVORAK_L1___________________,   _______,   _______________DVORAK_R1___________________, KC_BSPC,
       RGB_TOG, _______________DVORAK_L2___________________,              _______________DVORAK_R2___________________, RGB_TOG,
       RGB_MOD, _______________MACDVK_L3___________________,              _______________MACDVK_R3___________________, RGB_MOD,
       _______, _______, _______, ________MAC_THUMB_L______,   _______,   ________MAC_THUMB_R______, _______, _______, _______
     ),
-    [_QWERTY] = LAYOUT_split_4x6_2_wrapper(
+    [_QWERTY] = LAYOUT_wrapper(
       KC_ESC,  _________________COLEMAK_L1________________,    KC_ESC,   _________________QWERTY_R1_________________, KC_BSPC,
       KC_CAPS, _________________COLEMAK_L2________________,              _________________QWERTY_R2_________________, KC_ENT,
       KC_LSFT, _________________COLEMAK_L3________________,              _________________QWERTY_R3_________________, KC_RSFT,
       KC_LCTL, KC_LGUI, KC_LALT, _______, MO(_MNMB), MO(_SYMB), KC_SPC, MO(_SYMB), MO(_MNMB), KC_RGUI,_______, MO(_MDIA), MO(_FUNC)
     ),
-    [_COLBASE] = LAYOUT_split_4x6_2_wrapper(
+    [_COLBASE] = LAYOUT_wrapper(
       KC_ESC,  _______________DVORAK_L1___________________,   KC_MUTE,  _________________COLEMAK_R1________________, KC_BSPC,
       AU_TOG,  _______________DVORAK_L2___________________,             _________________COLEMAK_R2________________, RGB_TOG,
       CK_TOGG, _______________WINDVK_L3___________________,             _________________COLEMAK_R3________________, RGB_MOD,
@@ -47,28 +47,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     //Borderlands
-    [_GAMER] = LAYOUT_split_4x6_2_wrapper(
+    [_GAMER] = LAYOUT_wrapper(
       KC_ESC,  KC_R,    KC_G,    KC_W,    KC_E,    KC_1,     _______,    _________________RGB_1_____________________, KC_WINBASE,
       KC_TAB,  KC_V,    KC_A,    KC_S,    KC_D,    KC_2,                 _________________RGB_2_____________________, _______,
       KC_C,    KC_I,    KC_F,    KC_Q,    KC_4,    KC_3,                 _________________MEDIA_____________________, _______,
       KC_Z,    KC_X,    KC_K,    KC_LCTL, KC_LSFT, KC_SPC,   KC_SPC,     KC_F12,  KC_P,  KC_LEFT,  KC_UP,   KC_DOWN,  KC_RIGHT
     ),
 
-    [_SYMB] = LAYOUT_split_4x6_2_wrapper(
+    [_SYMB] = LAYOUT_wrapper(
       _______, _________________PUNC_L1_ALT_______________,    _______,  _________________PUNC_R1___________________, _______,
       _______, _________________PUNC_L3___________________,              _________________PUNC_R2___________________, _______,
       RGB_MOD, _________________PUNC_L3_ALT_______________,              _________________PUNC_R3___________________, _______,
       _______, _______, KC_NLCK, KC_SLCK, KC_DEL,  _______,     _______,  _______, KC_CAPS, BUZY,    _______, _______, _______
     ),
 
-    [_FUNC] = LAYOUT_split_4x6_2_wrapper(
+    [_FUNC] = LAYOUT_wrapper(
       _______, _____________FUNC_L1_______________________,    _______,  _______, _____________FUNC_1_______________, _______,
       _______, ________MAC_MISSION_CTRL__________, LGUI(KC_L),           _______, _____________FUNC_2_______________, _______,
       RGB_MOD, _____________FUNC_L3_______________________,              _______, _____________FUNC_3_______________, _______,
       _______, _______, _______, _______, _______, _______,    _______,  _______, KC_CAPS, _______, _______, _______, _______
     ),
 
-    [_MNMB] = LAYOUT_split_4x6_2_wrapper(
+    [_MNMB] = LAYOUT_wrapper(
       _______, _________________MACNAV_L1_________________,    _______,  _________________NUMB_R1___________________, _______,
       _______, _________________MACNAV_L2_________________,              _________________NUMB_R2___________________, _______,
       RGB_MOD, _________________MACNAV_L3_________________,              _________________NUMB_R3_MAC_______________, _______,
@@ -76,14 +76,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 
-    [_NUMB] = LAYOUT_split_4x6_2_wrapper(
+    [_NUMB] = LAYOUT_wrapper(
       _______, _________________WINNAV_L1_________________,    _______,  _________________NUMB_R1___________________, _______,
       _______, _________________WINNAV_L2_________________,              _________________NUMB_R2___________________, _______,
       RGB_MOD, _________________WINNAV_L3_________________,              _________________NUMB_R3_WIN_______________, _______,
       _______, _______, _______, TO(_WINBASE), _______, _______,    _______,  _______, RSFT_T(KC_ENT), KC_RSFT, _______, _______, _______
     ),
 
-    [_MDIA] = LAYOUT_split_4x6_2_wrapper(
+    [_MDIA] = LAYOUT_wrapper(
       _______, _________________LYOUT_____________________,    _______,  _________________RGB_1_____________________, RGB_TOG,
       _______, MU_TOG, _______, _______, _______,    CK_UP,              _________________RGB_2_____________________, _______,
       RGB_MOD, MU_MOD,  MU_TOG,  CK_TOGG,  CK_RST, CK_DOWN,              _________________MEDIA_____________________, _______,
