0d42d0bf1e725fb128ba2d5b3b284cec398dbb9f
Klackygears <37446827+Klackygears@users.noreply.github.com>
2023-03-30 10:54:02 -0400

    reset fingerpunch folder from fp_develop, identifying issues

--- keyboards/fingerpunch/fpm101/keymaps/fingertest/keymap.c
+++ keyboards/fingerpunch/fpm101/keymaps/fingertest/keymap.c
@@ -23,8 +23,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_fpm101(
     KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_LPRN,                                          KC_RPRN,   KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_BSPC,
-    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,                                                                   KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLS,
-    KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_LCBR,                                          KC_RCBR,   KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
+    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,                                                                   RGB_MOD,      RGB_HUI,      RGB_SPI,      RGB_SAI,      RGB_VAI,      RGB_TOG,
+    KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_LCBR,                                          KC_RCBR,   RGB_RMOD,     RGB_HUD,      RGB_SPD,      RGB_SAD,      RGB_VAD,   KC_QUOT,
     KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      ADJUST,                                           ADJUST,    KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_ENT,
                           KC_MUTE,   KC_LGUI,   LOWER,     KC_SPC,    KC_LALT,   KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3,    KC_RALT,   KC_SPC,    RAISE,     KC_RGUI,   KC_MUTE
 ),
@@ -98,5 +98,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
 
+    return true;
+}
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+      return false; /* Don't process further events if user function exists and returns false */
+    }
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_PGUP);
+        } else {
+            tap_code(KC_PGDN);
+        }
+    }
     return true;
 }
--- keyboards/fingerpunch/tenbit/readme.md
+++ keyboards/fingerpunch/tenbit/readme.md
@@ -1,12 +1,21 @@
-# enigma36
+# tenbit
 
-An ergonomic 30% unibody dactyl like keyboard
+An ortho 50% keyboard
 
 * Keyboard Maintainer: [sadekbaroudi](https://github.com/sadekbaroudi)
-* Hardware Supported: Pro Micro 5V/16MHz and compatible.
+
+tenbit
 
 Make example for this keyboard (after setting up your build environment):
+```
+    make fingerpunch/tenbit:default RGB_MATRIX_ENABLE=yes CIRQUE_ENABLE=yes AUDIO_ENABLE=yes
+```
 
-    make fingerpunch/personal/handwired/enigma36/v1:default
+Options are:
+```
+    CIRQUE_ENABLE=yes
+    RGB_MATRIX_ENABLE=yes
+    AUDIO_ENABLE=yes
+```
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
